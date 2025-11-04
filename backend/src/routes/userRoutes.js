0fb55aef931f4d39dbce3aac39fc3f84c1f3a55b
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2025-11-04 10:16:28 -0700

    add login import

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
