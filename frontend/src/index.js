4b2565123e636065fb472fae05c13d21a79ed538
heathergibb <hrgibb@gmail.com>
2025-11-06 16:38:55 -0700

    redirect root to login page if no user logged in

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,27 @@ import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LoginForm, RegisterForm } from "./components/Form/Form";
 
+// Check if a user is logged in 
+const isLoggedIn = !!localStorage.getItem("token");
+
+// ProtectedRoute only renders children if user is logged in
+function ProtectedRoute({ children }) {
+  return isLoggedIn ? children : window.location.href = "/login";
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={App} />
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <App />
+            </ProtectedRoute>
+          } 
+        />
         <Route path="/login" Component={LoginForm} />
         <Route path="/register" Component={RegisterForm} />
       </Routes>
