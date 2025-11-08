f75931ba0014e309c87f83784c137875e7f346b4
heathergibb <hrgibb@gmail.com>
2025-11-07 17:02:06 -0700

    fixed index.js to handle header/footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,27 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginForm, RegisterForm } from "./components/form/Form";
 import Layout from "./components/layout/Layout";
-import { LoginForm, RegisterForm } from "./components/Form/Form";
 
-// Check if a user is logged in 
+// Check if a user is logged in
 const isLoggedIn = !!localStorage.getItem("token");
 
 // ProtectedRoute only renders children if user is logged in
 function ProtectedRoute({ children }) {
-  return isLoggedIn ? children : window.location.href = "/login";
+  return isLoggedIn ? children : (window.location.href = "/login");
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes element={<Layout />}>
-        <Route path="/" Component={App} />
-        <Route path="/login" Component={LoginForm} />
-        <Route path="/register" Component={RegisterForm} />
+      <Routes>
+        {/* Layout wraps everything so header/footer always show */}
+        <Route path="/" element={<Layout />}>
+          {/* Protected route for main app */}
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <App />
+              </ProtectedRoute>
+            }
+          />
+          {/* Public routes */}
+          <Route path="login" element={<LoginForm />} />
+          <Route path="register" element={<RegisterForm />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
